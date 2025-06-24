baae9d4c1a09a59469e045e8b99be7b14b9c4113
SamuelMaisDelivery <samuel@maisdelivery.com.br>
2025-06-24 00:15:52 -0300

    SUBIU

--- Back-End/src/main/java/com/api/BigCondominio/config/CorsConfig.java
+++ Back-End/src/main/java/com/api/BigCondominio/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500") // não pode ser * pois o security barra.
+                        .allowedOrigins("http://127.0.0.1:5500", "https://front-bigcondominios-jp79.vercel.app/") // não pode ser * pois o security barra.
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
